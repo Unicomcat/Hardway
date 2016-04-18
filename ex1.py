0a8c754402831b353de57fad19a1c67bae01b77e
unicomcat <unicomcat@outlook.com>
2016-04-18 16:49:01 +0800

    finish ex6.py

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello world!"
+print "Hello Again"
+print "i linke typing this."
+print "This is fun."
+print "yay! Printing."
+print "i'd much rather you 'not'."
+print 'i"saind" do not touch this'
---
+++ ex2.py
@@ -0,0 +1,6 @@
+# A comment, this is so you can read your program later.
+#Angything after the # is ignored by python.
+print "i could have code like this." # and the comment after is ignored
+#You can also use a comment to "disable" or comment out a piece of code:
+# print "this Won't run."
+print "This will run."
