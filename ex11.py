08f05b3618a59b33fced2cb80e1a2d257dcfb2ec
unicomcat <unicomcat@outlook.com>
2016-04-20 01:10:43 +0800

    finish ex14.py

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight =  raw_input()
+print "So, you're %r old, %r tall and %r heavy." %(age, height, weight)
+
+
+print '''
+We put ,(comma) at the end of each print line. This is so  print doesn't end the line
+with a newline character and go to the next line.
+'''
