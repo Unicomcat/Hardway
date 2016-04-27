a39e18c4998096482e34d44f7b5ee4ceb4a098c9
unicomcat <unicomcat@outlook.com>
2016-04-27 16:57:06 +0800

    finish ex41.py

--- oop_test.py
+++ oop_test.py
@@ -3,15 +3,15 @@ from urllib import urlopen
 import sys
 
 WORD_URL = 'http://learncodethehardway.org/words.txt'
-WORD = []
+WORDS = []
 
 PHRASES = {
 	"class %%%(%%%):":
-	"Make a class name %%% that is-a %%%."
+	"Make a class name %%% that is-a %%%.",
 	"class %%%(object):\n\tdef __init__(self, ***)":
-	"class %%% has-a __init__ that takes self and *** parameters."
+	"class %%% has-a __init__ that takes self and *** parameters.",
 	"class %%%(object):\n\tdef ***(self,@@@)":
-	"class %%% has-a function named *** that takes self and @@@ parameters."
+	"class %%% has-a function named *** that takes self and @@@ parameters.",
 	"*** = %%%()":
 	"Set *** to an instance of class %%%.",
 	"***.***(@@@)":
@@ -23,9 +23,9 @@ PHRASES = {
 
 # do they want to drill phrases first
 if len(sys.argv) == 2 and sys.argv[1] == "english":
-	PHRASE_FIRST = True
+    PHRASE_FIRST = True
 else:
-	PHRASE_FIRST = False
+    PHRASE_FIRST = False
 
 # load up the words from the website
 for word in urlopen(WORD_URL).readlines():
@@ -38,4 +38,41 @@ def convert(snippet, phrase):
 	param_names =[]
 
 	for i in range(0, snippet.count('@@@')):
-		param_count = ran
+		param_count = random.randint(1,3)
+		param_names.append(', '.join(random.sample(WORDS, param_count)))
+
+	for sentence in snippet, phrase:
+		result = sentence[:]
+
+		#fake class param_names
+		for word in class_names:
+			result = result.replace("%%%", word, 1)
+
+		#fake other param_names
+		for word in other_names:
+			result = result.replace("***", word, 1)
+
+		#fake parameter lists
+		for word in param_names:
+			result = result.replace("@@@", word, 1)
+
+		results.append(result)
+	return results
+
+#keep going until they hit CTRL-D
+try:
+	while True:
+		snippets = PHRASES.keys()
+		random.shuffle(snippets)
+
+		for snippet in snippets:
+			phrase = PHRASES[snippet]
+			question, answer = convert(snippet, phrase)
+			if PHRASE_FIRST:
+				question, answer = answer, question
+			print question
+
+			raw_input("> ")
+			print "ANSWER: %s\n\n" % answer
+except EOFError:
+    print "\nbye"
